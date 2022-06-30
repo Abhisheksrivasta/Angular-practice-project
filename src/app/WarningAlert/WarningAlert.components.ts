5b731e3be80ab7129714a9991bf11dc63ccf4c46
raunak-io <fullstackerr@gmail.com>
2022-06-30 06:34:12 +0530

    make 2 new components 1 is manually ans second is with command

---
+++ src/app/WarningAlert/WarningAlert.components.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+
+
+@Component({
+selector:'Warning-Alert',
+templateUrl:'./WarningAlert.components.html',
+styleUrls:['./WarningAlert.components.css']
+
+
+})
+
+export class WarningAlert{
+
+}
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { WarningAlert } from './WarningAlert/WarningAlert.components';
 import { ServerComponent } from './server/server.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -5,10 +6,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServersComponent } from './servers/servers.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 
 @NgModule({
-  declarations: [AppComponent,ServerComponent, ServersComponent],
+  declarations: [AppComponent,ServerComponent, ServersComponent, SuccessAlertComponent,WarningAlert],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
