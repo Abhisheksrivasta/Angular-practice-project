caca7b860734ae476ddd6671d7cca7e0e48cc692
raunak-io <fullstackerr@gmail.com>
2022-07-03 10:22:20 +0530

    Assignment 36

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServersComponent } from './servers/servers.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [AppComponent,ServerComponent, ServersComponent, SuccessAlertComponent,WarningAlert],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule,FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,5 +1,6 @@
 import { AppComponent } from './../app.component';
 import { Component, OnInit } from '@angular/core';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-servers',
@@ -10,6 +11,11 @@ styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
  allowNewServer=false
+ serverCreationStatus ='No server was created'
+ serverName ='Testserver';
+
+
+  userName='abhishek'
   constructor() {
     setTimeout(()=>{
       this.allowNewServer=true
@@ -18,5 +24,17 @@ export class ServersComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  onCreateServer(){
+
+    this.serverCreationStatus='Server was created !Name is ' + this.serverName
+    this.userName=''
+  }
+
+onUpdateServerName(event:Event){
+this.serverName=(<HTMLInputElement>event.target).value;
+// this.userName=(<HTMLInputElement>event.target).value;
+}
+
 
 }
